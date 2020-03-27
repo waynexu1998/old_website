b299003c3e63121f8653e9d547d164a20b9a394e
wayneXu_Razer <jxu51@u.rochester.edu>
2020-03-26 22:36:37 -0400

    init

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.navDash')
+let menuOpen = false
+
+
+if(hamburger) {
+    hamburger.addEventListener('click', () => {
+        if(!menuOpen) {
+            hamburger.classList.add('open')
+            menuOpen = true
+        } else {
+            hamburger.classList.remove('open')
+            menuOpen = false
+        }
+    })
+}
