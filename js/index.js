cec90880953c40b0d38c27ede6ba13dcb0a888f9
wayneXu_Razer <jxu51@u.rochester.edu>
2020-04-14 19:02:35 -0400

    animations

--- js/index.js
+++ js/index.js
@@ -13,3 +13,32 @@ if(hamburger) {
         }
     })
 }
+
+const timeline = gsap.timeline({
+    pasuded: true
+})
+
+timeline.from('.giant_title', {
+    y: '100%',
+    duration: 1,
+    opacity: 0,
+    ease: 'power4.in'
+})
+
+timeline.from('.giant_title2', {
+    opacity: 0,
+    duration: 0.5
+})
+
+timeline.from('.navigation', {
+    opacity: 0,
+    duration:0.5
+},
+'-=0.5')
+
+timeline.from('.editor', {
+    duration: 0.5,
+    opacity: 0,
+    ease: 'power4.in'
+},
+'-=0.2')
