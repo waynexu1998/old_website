54f585f8b55866ffc87272158b25ed12cef5d2c1
wayneXu_Razer <jxu51@u.rochester.edu>
2020-04-14 23:52:10 -0400

    adding me section

--- js/index.js
+++ js/index.js
@@ -14,9 +14,8 @@ if(hamburger) {
     })
 }
 
-const timeline = gsap.timeline({
-    pasuded: true
-})
+// hero section animtion
+const timeline = gsap.timeline()
 
 timeline.from('.giant_title', {
     y: '100%',
@@ -37,8 +36,46 @@ timeline.from('.navigation', {
 '-=0.5')
 
 timeline.from('.editor', {
-    duration: 0.5,
+    duration: 0.3,
     opacity: 0,
     ease: 'power4.in'
 },
 '-=0.2')
+
+// hero fade out
+const timeline2 = gsap.timeline({
+    paused: true
+})
+
+timeline2.to('.divider', {
+    opacity:0,
+    duration: 1.6,
+    ease: 'power4.out'
+})
+timeline2.to('.divider2', {
+    opacity:0,
+    duration: 1.6,
+    ease: 'power4.out'
+},
+'-=1.6')
+timeline2.to('.editor', {
+    y: -1000,
+    opacity:0,
+    duration:1.6,
+    ease: 'power4.out'
+},'-=1.6')
+timeline2.to('.title_wrapper', {
+    opacity:0,
+    duration:1.6,
+    ease: 'power4.out'
+},'-=1.6')
+
+const controller = new ScrollMagic.Controller()
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.HERO',
+    duration: 1600,
+    triggerHook: 0
+})
+.setTween(timeline2)
+.addIndicators()
+.addTo(controller)
