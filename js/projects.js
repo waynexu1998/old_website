284f8dce7175287c112e36b24fe9cbc26b0f15a3
Wayne <34214179+waynexu1998@users.noreply.github.com>
2020-05-11 17:10:07 -0400

    gsap for projects page

---
+++ js/projects.js
@@ -0,0 +1,34 @@
+const timeline = gsap.timeline({
+    paused: true
+})
+
+timeline.from('.move_title',{
+    x: 30
+})
+
+const controller = new ScrollMagic.Controller()
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.navBar',
+    duration: 500,
+    triggerHook: 0
+})
+.setTween(timeline)
+// .addIndicators()
+.addTo(controller)
+
+
+const timeline2 = gsap.timeline({
+    paused: true
+})
+
+timeline.from('.move_title2',{
+    x: -30
+})
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: '.PJ2',
+    duration: 500,
+})
+.setTween(timeline2)
+// .addIndicators()
+.addTo(controller)
