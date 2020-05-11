4bf6c780a2aab851333acd0bad050887aca893f3
Wayne <34214179+waynexu1998@users.noreply.github.com>
2020-05-11 18:04:27 -0400

    change animation

--- js/projects.js
+++ js/projects.js
@@ -1,37 +1,17 @@
 const timeline = gsap.timeline({
-    paused: true
 })
 
 timeline.from('.move_title',{
-    x: 30,
-    ease: 'power4.in'
+    x: 200,
+    ease: 'power4.out'
 
 })
-
-const controller = new ScrollMagic.Controller()
-const scene = new ScrollMagic.Scene({
-    triggerElement: '.navBar',
-    duration: 500,
-    triggerHook: 0
-})
-.setTween(timeline)
-// .addIndicators()
-.addTo(controller)
 
 
 const timeline2 = gsap.timeline({
-    paused: true
 })
 
 timeline.from('.move_title2',{
-    x: -30,
-    ease: 'power4.in'
+    x: -200,
+    ease: 'power4.out'
 })
-
-const scene2 = new ScrollMagic.Scene({
-    triggerElement: '.PJ2',
-    duration: 500,
-})
-.setTween(timeline2)
-// .addIndicators()
-.addTo(controller)
