cf3623092a33bd4c0cdca06f3a5c7d0034c69a4b
Wayne <34214179+waynexu1998@users.noreply.github.com>
2020-05-11 22:04:05 -0400

    woah magic

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,7 @@
+const PJ1_image_wrapper = document.querySelector('.PJ1_image_wrapper')
+const PJ2_image_wrapper = document.querySelector('.PJ2_image_wrapper')
+const move_title = document.querySelector('.move_title')
+
 function anim_start() {
     timeline.play()
     timeline2.play()
@@ -18,9 +22,46 @@ const timeline2 = gsap.timeline({
     paused: true
 })
 
-timeline.from('.move_title2',{
+timeline2.from('.move_title2',{
     x: -200,
     ease: 'power4.out'
 })
 
+const timeline3 = gsap.timeline({
+    paused: true
+})
+
+timeline3.to('.move_title',{
+    x: 150,
+})
+
+const timeline4 = gsap.timeline({
+    paused: true
+})
+
+timeline4.to('.move_title2',{
+    x: -150,
+})
+
+
 window.addEventListener("load", setTimeout(anim_start, 300))
+
+function PJ1_hover() {
+    PJ1_image_wrapper.style.transform = "rotateY(30deg)"
+    timeline3.play()
+}
+
+function PJ2_hover() {
+    PJ2_image_wrapper.style.transform = "rotateY(-30deg)"
+    timeline4.play()
+}
+
+function PJ1_hover_out() {
+    PJ1_image_wrapper.style.transform = "initial"
+    timeline3.reverse()
+}
+
+function PJ2_hover_out() {
+    PJ2_image_wrapper.style.transform = "initial"
+    timeline4.reverse()
+}
