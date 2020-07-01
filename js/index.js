0561b6d2c236b657740320349dd442b4850c0210
Wayne <34214179+waynexu1998@users.noreply.github.com>
2020-07-01 13:51:04 -0400

    aniamtion for index

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 const video = document.querySelector(".video")
+const title = document.querySelector(".HERO_title")
 
 $(window).on("load",function(){
     setTimeout(function() {
         $(".load_page").fadeOut("slow");
         video.play()
+        timeline.play()
     },1000)
 });
+
+
+  //AOS init and config
+AOS.init({
+    once: true,
+    easing: 'ease-out-quad',
+})
+
+const timeline = gsap.timeline({
+    paused:true,
+})
+
+timeline.from(title, {
+    y: '100%',
+    ease: 'power.out',
+    duration: 2,
+})
